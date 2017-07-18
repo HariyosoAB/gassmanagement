64d8b6209713a131e1fbb354f5eb8db6790e761a
DESKTOP-O15N8BP\shafly <shaflynaufal96@gmail.com>
2017-07-18 09:42:45 +0700

    kampang

---
+++ app/Http/Controllers/umumController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class umumController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/tes');
 });
 
 Route::get('login','userController@showlogin');
