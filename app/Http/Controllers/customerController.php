4d388d93a3fef0257e8f8d38c3a519ff162d2526
Ario Bimo <hariyosoariobimo@gmail.com>
2017-07-21 09:17:21 +0700

    tesgetprocess

--- app/Http/Controllers/customerController.php
+++ app/Http/Controllers/customerController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Order;
 use Redirect;
 use Auth;
+use DB;
 class customerController extends Controller
 {
     public function orderForm(){
@@ -32,6 +33,18 @@ class customerController extends Controller
         $order->save();
 
         return Redirect('/cust/progress')->with('success','You have created a new order');
-
     }
+    public function viewprogress()
+    {
+      $data['progress'] = DB::table('order_f')
+        ->join('equipment','order_f.order_equipment','=','equipment.equipment_id')
+        ->join('actype','order_f.order_ac_type','=','actype.actype_id')
+        ->join('urgency','order_f.order_urgency','=','urgency.urgency_id')
+        ->join('airline','airline.airline_id','=','order_f.order_airline')
+        ->where('order_f.order_status','!=','3')
+        ->where('order_f.order_user','=',Auth::user()->user_id)
+        ->get();
+      return view('pages.customer.progress',$data);
+    }
+
 }
